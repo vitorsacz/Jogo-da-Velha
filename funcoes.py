2c4da701c8abe68f101214c56b614f886f2be0ae
Vitor Santos <vitorsacz@gmail.com>
2023-10-31 10:49:58 -0300

    Inserindo as DEF's

---
+++ funcoes.py
@@ -0,0 +1,26 @@
+
+
+def inicializar_tabuleiro():
+    pass
+
+
+def exibir_tabuleiro(tabuleiro):
+    pass
+
+def fazer_jogada():
+    pass
+
+
+def verificar_vencedor():
+    pass
+
+
+def jogar_novamente():
+    pass
+
+
+def pula(linha):
+    for i in range(linha):
+        print("\n")
+
+
