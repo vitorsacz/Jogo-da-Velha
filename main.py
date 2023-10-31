f095a4272bb7c6acfe6b502c334b09d983525f03
Vitor Santos <vitorsacz@gmail.com>
2023-10-31 12:46:24 -0300

    código principal

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import funcoes
 
 
-def main():
-    pass
+cor_vermelha = '\033[91m'
+cor_verde = '\033[92m'
+cor_azul = '\033[94m'
+cor_reset = '\033[0m'  # Reseta a cor para a padrão
 
+def main():
+
+    op = funcoes.cabecalho()
+
+    if (op == "S") or (op == "s") or (op == "Sim") or (op == "sim"):
+        while True:
+            funcoes.explicacao_game()
+            
+            break
+    else:
+        print("Obrigado por participar conosco")
+        
+
+    
 
 if __name__ == "__main__":
     main()
